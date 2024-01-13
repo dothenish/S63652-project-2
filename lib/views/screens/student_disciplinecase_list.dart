84d24043918f8b0785e2f0980306f7fd2097113d
Nisha Farhani Khirul Fozi <dothenish@gmail.com>
2024-01-14 02:58:15 +0800

    changes

--- lib/views/screens/student_disciplinecase_list.dart
+++ lib/views/screens/student_disciplinecase_list.dart
@@ -8,8 +8,23 @@ class StudentDisciplineCasePage extends StatefulWidget {
 }
 
 class _StudentDisciplineCasePageState extends State<StudentDisciplineCasePage> {
+
+    void navigateToAddStudentDisciplineCasePage() {
+      Navigator.pushReplacementNamed(context, '/add_student_case_page');
+    }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[800],
+        title: const Text('Student Discipline Case'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: navigateToAddStudentDisciplineCasePage,
+        backgroundColor: Colors.deepPurple[800],
+        child: const Icon(Icons.add),
+      )
+    );
   }
 }
--- lib/views/screens/student_result_list_page.dart
+++ lib/views/screens/student_result_list_page.dart
@@ -8,8 +8,27 @@ class StudentResultPage extends StatefulWidget {
 }
 
 class _StudentResultPageState extends State<StudentResultPage> {
+
+    void navigateToAddStudentResultPage() {
+      Navigator.pushReplacementNamed(context, '/add_student_result_page');
+      }
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[800],
+        title: const Text('Student Result'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: navigateToAddStudentResultPage,
+        backgroundColor: Colors.deepPurple[800],
+        child: const Icon(Icons.add),
+      )
+    );
   }
 }
